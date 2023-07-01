fdac6228eb75b2bf201f33d3ca7d616ee9ea3e97
Tyler <tylermartel@outlook.com>
2023-06-30 20:46:03 -0400

    added function that gets the unique colors of an image

--- palette/palette.go
+++ palette/palette.go
@@ -2,8 +2,19 @@ package palette
 
 import (
 	"image"
+	"image/color"
+	_ "image/jpeg"
 	"os"
 )
+
+type Pixel struct {
+	x, y int
+}
+
+type Color struct {
+	color  color.RGBA
+	pixels []Pixel
+}
 
 func loadImage(imagePath string) image.Image {
 	file, err := os.Open(imagePath)
@@ -20,9 +31,30 @@ func loadImage(imagePath string) image.Image {
 	return image
 }
 
+func getColors(image image.Image) []Color {
+	width, height := image.Bounds().Max.X, image.Bounds().Max.Y
+	colorToPixelsMap := make(map[color.RGBA][]Pixel)
+
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			pixel := Pixel{x: x, y: y}
+			color := color.RGBAModel.Convert(image.At(x, y)).(color.RGBA)
+			colorToPixelsMap[color] = append(colorToPixelsMap[color], pixel)
+		}
+	}
+
+	colors := make([]Color, 0)
+	for color, pixels := range colorToPixelsMap {
+		colors = append(colors, Color{color: color, pixels: pixels})
+	}
+
+	return colors
+}
+
 func Create(imagePaths []string, k int) string {
 	for _, imagePath := range imagePaths {
-		loadImage(imagePath)
+		image := loadImage(imagePath)
+		getColors(image)
 	}
 
 	return "Well met!"
